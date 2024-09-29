38f61e4dc09138c579c844f9a6d7ccc0766b86f0
shivam tiwari <dev.shivamtiwari@gmail.com>
2024-09-29 11:32:32 +0530

    texr area resize function

--- src/utils/downloadCanvas.js
+++ src/utils/downloadCanvas.js
@@ -1,4 +1,6 @@
 import html2canvas from 'html2canvas'
+import { toPng } from 'html-to-image'
+import domtoimage from 'dom-to-image'
 
 export const downloadCanvas = (elementId) => {
   const canvasElement = document.getElementById(elementId) // The div wrapping your canvas.
@@ -12,4 +14,37 @@ export const downloadCanvas = (elementId) => {
     link.download = 'screenshot.png'
     link.click()
   })
+}
+
+export const downloadPngImage = (elementId) => {
+  const node = document.getElementById(elementId)
+
+  toPng(node)
+    .then((dataUrl) => {
+      const link = document.createElement('a')
+      link.download = 'image.png'
+      link.href = dataUrl
+      link.click()
+    })
+    .catch((error) => {
+      console.error('Failed to create image:', error)
+    })
+}
+
+
+
+export const downloadDOMImage = (elementId) => {
+  const node = document.getElementById(elementId) // Ensure to use the correct ID
+
+  domtoimage
+    .toPng(node)
+    .then((dataUrl) => {
+      const link = document.createElement('a')
+      link.download = 'image.png'
+      link.href = dataUrl
+      link.click()
+    })
+    .catch((error) => {
+      console.error('Oops, something went wrong!', error)
+    })
 }
--- src/utils/resizeTextArea.js
+++ src/utils/resizeTextArea.js
@@ -1,8 +1,6 @@
 export const resizeTextArea = (id, minWidth, minHeight) => {
   const textarea = document.getElementById(id)
   if (textarea) {
-    // textarea.style.minWidth = `${minWidth}px`
-    // textarea.style.minHeight = `${minHeight}px`
     textarea.style.height = 'auto'
     textarea.style.height = `${textarea.scrollHeight}px`
   }
