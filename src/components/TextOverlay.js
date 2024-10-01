cac60c718b6af64a8cf142f9bf6a8707e1ae43e3
shivam tiwari <dev.shivamtiwari@gmail.com>
2024-10-01 05:55:48 +0530

    opacity balance

--- src/components/TextOverlay.js
+++ src/components/TextOverlay.js
@@ -4,7 +4,9 @@ const TextOverlay = ({
   bgImage,
   text,
   setText,
-  fontColor,
+  textColor,
+  textSize,
+  textStyle,
   bgOpacity,
   textOpacity,
   width,
@@ -13,10 +15,8 @@ const TextOverlay = ({
   const textRef = useRef(null)
 
   useEffect(() => {
-    if (textRef.current) {
-      textRef.current.style.color = fontColor
-    }
-  }, [fontColor, text])
+    if (textRef.current) textRef.current.style.color = textColor
+  }, [textColor])
 
   return (
     <div
@@ -26,7 +26,6 @@ const TextOverlay = ({
         height: `${height}px`,
       }}
     >
-      {/* Background with opacity */}
       <div
         style={{
           backgroundImage: `url(${bgImage})`,
@@ -36,10 +35,19 @@ const TextOverlay = ({
           position: 'absolute',
           width: '100%',
           height: '100%',
+          pointerEvents: 'none',
         }}
       />
 
-      {/* Text area with independent opacity */}
+      <div
+        className='absolute w-full h-full'
+        style={{
+          backgroundColor: `rgba(0, 0, 0, ${0.5})`,
+          opacity: 1,
+          pointerEvents: 'none',
+        }}
+      />
+
       <textarea
         ref={textRef}
         value={text}
@@ -47,9 +55,13 @@ const TextOverlay = ({
         className='absolute text-center outline-none border-none resize-none w-full h-full px-2 py-2'
         style={{
           fontFamily: 'ABeeZee',
-          fontSize: '24px',
+          fontStyle: textStyle,
+          fontSize: textSize,
+          fontColor: textColor,
           opacity: textOpacity,
           pointerEvents: 'auto',
+          textShadow: '2px 2px 4px rgba(0, 0, 0, 0.8)',
+          zIndex: 1,
         }}
       />
     </div>
