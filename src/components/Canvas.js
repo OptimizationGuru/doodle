78679d2c8eedc7539393a3560658e2058a4cc6a9
shivam tiwari <dev.shivamtiwari@gmail.com>
2024-10-08 03:09:12 +0530

    borders removed

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -142,11 +142,11 @@ const Canvas = () => {
   }, [pageNumber, imgCategory, galleryImages])
 
   return (
-    <div className='w-full h-full pb-[95px] min-h-screen bg-[#121212] flex flex-col justify-center items-center border-[1px] border-blue-400 overflow-y-hidden'>
+    <div className='w-full h-full pb-[95px] min-h-screen bg-[#121212] flex flex-col justify-center items-center overflow-y-hidden'>
       <div
         className={`grid fixed top-0 z-20 right-0  bg-black h-auto justify-center items-center border-white border-[1px] xs:flex xs:flex-col xs:justify-center  xs:w-full sm:grid-cols-12 sm:w-full sm:-mb-[1000px] md:grid-cols-12  lg:grid-cols-8 xl:grid-cols-12 2xl:grid-cols-12`}
       >
-        <div className='flex justify-center items-center h-auto  py-2  gap-3  px-2 sm:col-span-12 md:col-span-6 lg:col-span-3 lg:ml-[250px]  xl:col-span-2 xl:ml-8 2xl:ml-24 2xl:col-span-2 border-blue-500 border-[1px]'>
+        <div className='flex justify-center items-center h-auto  py-2  gap-3  px-2 sm:col-span-12 md:col-span-6 lg:col-span-3 lg:ml-[250px]  xl:col-span-2 xl:ml-8 2xl:ml-24 2xl:col-span-2 '>
           <div className='w-auto h-auto relative' ref={textPickerRef}>
             <button
               onClick={toggleTextColorPicker}
@@ -209,7 +209,7 @@ const Canvas = () => {
           </div>
         </div>
 
-        <div className='flex items-center justify-center gap-2  px-2  py-1 sm:col-span-12 md:col-span-6 lg:col-span-3 lg:ml-[120px] xl:col-span-4  2xl:col-span-4 border-blue-500 border-[1px]'>
+        <div className='flex items-center justify-center gap-2  px-2  py-1 sm:col-span-12 md:col-span-6 lg:col-span-3 lg:ml-[120px] xl:col-span-4  2xl:col-span-4 '>
           <div className='w-auto h-auto flex justify-center py-1 '>
             <FontSizeSelector onSelect={handleFontSizeSelect} />
           </div>
@@ -223,7 +223,7 @@ const Canvas = () => {
           </div>
         </div>
 
-        <div className='flex gap-2 items-center justify-center px-2 py-1 xs:overflow-x-auto xs:flex-wrap xs:justify-evenly xs:gap-0 sm:col-span-12 md:col-span-8 md:ml-[250px] lg:col-span-8 xl:col-span-5 xl:ml-24 2xl:w-[auto] 2xl:col-span-5 border-blue-500 border-[1px]'>
+        <div className='flex gap-2 items-center justify-center px-2 py-1  xs:flex-wrap xs:justify-evenly xs:gap-0 sm:col-span-12 md:col-span-8 md:ml-[250px] lg:col-span-8 xl:col-span-5 xl:ml-24 2xl:w-[auto] 2xl:col-span-5 '>
           <div className='w-auto h-auto flex justify-center items-center py-1'>
             <div className='w-auto h-auto flex justify-center items-center py-1 mx-1'>
               <BannerSizeSelector onSelect={handleBannerSizeSelect} />
@@ -269,7 +269,7 @@ const Canvas = () => {
 
       <div
         id='canvas-container'
-        className='flex items-center overflow-y-hidden justify-center overflow-auto w-auto h-full border-blue-400 border-[1px]'
+        className='flex items-center overflow-y-hidden justify-center w-auto h-full '
         style={{
           width: !selectedImage && width ? `${width}px` : 'auto',
           height: !selectedImage && height ? `${height}px` : 'auto',
@@ -300,7 +300,7 @@ const Canvas = () => {
           //banner
           <div className='flex flex-col gap-4 w-full h-full item-center justify-center sm:mt-[100px] md:mt-[10px]'>
             {!selectedImage && showGallery && (
-              <div className='w-auto justify-center border-blue-400 border-[1px] text-center text-wrap p-4 rounded-lg shadow-md h-auto xs:text-sm xs:w-1/2 xs:mt-32 xs:mx-auto xs:text-wrap sm:w-2/3 sm:mt-32 sm:mx-auto sm:text-wrap'>
+              <div className='w-auto justify-center  text-center text-wrap p-4 rounded-lg shadow-md h-auto xs:text-sm xs:w-1/2 xs:mt-32 xs:mx-auto xs:text-wrap sm:w-2/3 sm:mt-32 sm:mx-auto sm:text-wrap'>
                 <p className='text-white text-center text-lg xs:text-sm xs:w-1/2 xs:mt-12 xs:mx-auto xs:text-wrap'>
                   {galleryImages.length > 0 ? displayText : display_sorry_msg}
                 </p>
@@ -327,7 +327,7 @@ const Canvas = () => {
       </div>
 
       <div
-        className={`w-auto p-2 my-4 flex items-center gap-4 text-center justify-center  overflow-y-hidden border-blue-600 border-[1px]  ${
+        className={`w-auto p-2 my-4 flex items-center gap-4 text-center justify-center overflow-y-hidden   ${
           !selectedImage ? 'hidden' : ''
         }`}
       >
@@ -345,7 +345,7 @@ const Canvas = () => {
         </div>
       </div>
 
-      <div className='flex w-full overflow-x-auto gap-2 mt-4 border-blue-400 border-[1px]  sm:w-full'>
+      <div className=' w-full h-auto flex items-center justify-center  ml-12 px-4 overflow-x-auto gap-4 mt-4 '>
         {showGallery &&
           galleryImages.map((image) => (
             <img
